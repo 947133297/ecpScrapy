df42057ada33d86defb4c5b167662d9c3e122b7f
947133297 <947133297@qq.com>
2018-07-20 23:40:09 +0800

    添加题库页面展示和数据解析

---
+++ parserSrv/index.js
@@ -0,0 +1,69 @@
+const rf = require("fs");
+const http = require('http');
+const url = require('url');
+
+rf.readFile("../log.txt", 'utf-8', function (err, data) {
+    let arr = data.split("##6685##");
+    handleSingleItem(arr[1]);
+    let items = {};
+    arr.forEach((val) => {
+        if (val.trim().length === 0) {
+            return;
+        }
+        let target = handleSingleItem(val);
+        items[target.question] = target;
+    });
+    openSrv(Object.keys(items).map((val) => {
+        return items[val];
+    }));
+});
+
+function handleSingleItem(val) {
+    let lines = val.trim().split('\r\n');
+    let head = lines.splice(0, 1)[0];
+    let answerIndex = head.lastIndexOf("正确答案");
+    let question = head.substr(0, answerIndex);
+    let answer = head.substr(answerIndex);
+    let content = lines.reduce((res, val) => {
+        return res + "<br />" + val;
+    });
+    return {
+        question, answer, content
+    }
+}
+
+function openSrv(items) {
+    http.createServer(function (req, res) {
+        if (req.url === "/size") {
+            resp(res, {
+                size: items.length
+            })
+        } else if (req.url.startsWith("/fetch")) {
+            let index = url.parse(req.url, true).query.index;
+            resp(res, items[index]);
+        } else if (req.url.startsWith("/page")) {
+            let page = rf.readFileSync("index.html", "utf-8");
+            respHtml(res, page);
+        }
+
+    }).listen(8888);
+
+    console.log('服务器开启成功');
+}
+
+function resp(res, json) {
+    res.writeHeader(200, {
+        'content-type': 'text/json;charset="utf-8"'
+    });
+    res.write(JSON.stringify(json));
+    res.end();
+}
+
+
+function respHtml(res, page) {
+    res.writeHeader(200, {
+        'content-type': 'text/html;charset="utf-8"'
+    });
+    res.write(page);
+    res.end();
+}
